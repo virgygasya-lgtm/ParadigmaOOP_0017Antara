374c7eaa7300f740b5dcd0fbe0378b8caa528140
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:27:28 +0700

    membuatclassbudi.cpp

--- membuatclassbudi.cpp
+++ membuatclassbudi.cpp
@@ -76,3 +76,48 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
