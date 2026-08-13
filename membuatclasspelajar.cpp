9f6eb42669eee17d7f87ac69d2ae5c19ee95c92e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:26:39 +0700

    membuatclasspelajar.cpp

--- membuatclasspelajar.cpp
+++ membuatclasspelajar.cpp
@@ -44,3 +44,35 @@ public:
         cout << "pekerja dibuat\n" << endl;
     }
 };
+
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
