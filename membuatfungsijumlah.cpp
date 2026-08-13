0bd53e6ef0716d6a62e87ca680d59bd6ba68bbf9
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:12:14 +0700

    membuatfungsijumlah.cpp

--- membuatfungsijumlah.cpp
+++ membuatfungsijumlah.cpp
@@ -45,3 +45,27 @@ public:
         ~orang() {
         cout << "Orang dihapus\n" << endl;
     }
+
+    #include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variable dari class orang
+    string nama;
+
+        //pembuatan constructor dan destructor dari class orang
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+        ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+        //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
