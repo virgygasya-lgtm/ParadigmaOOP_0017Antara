337140a18d70ac86e0c1ef1f3d429360a423ffaf
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:43:09 +0700

    membuatclasspekerja.cpp

--- membuatclasspekerja.cpp
+++ membuatclasspekerja.cpp
@@ -11,3 +11,12 @@ public:
         cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
