0b2e3aad957d4322846789bfaddc968f2b245b23
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:09:39 +0700

    membuatclassorang.cpp

---
+++ membuatclassorang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variable dari class orang
+    string nama;
