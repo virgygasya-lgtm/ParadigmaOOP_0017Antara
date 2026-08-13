99bd5057782217677f2e806d7748ce94a6aa7a8e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 14:55:05 +0700

    MenambahkanMethodUntukMenampilkanNilai.cpp

--- MembuatClassDanPrivateMember.cpp
+++ MembuatClassDanPrivateMember.cpp
@@ -29,3 +29,49 @@ public:
         y = b;
     }
 };
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    string x, y;
+}; 
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+};
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    // menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
