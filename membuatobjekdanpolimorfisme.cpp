b7659d7719f1cd0ac712a200effe019280724662
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:38:45 +0700

    membuatobjekdanpolimorfisme.cpp

--- membuatobjekdanpolimorfisme.cpp
+++ membuatobjekdanpolimorfisme.cpp
@@ -19,3 +19,17 @@ public:
         cout << "Pesan dari lia" << endl;
     }
 };
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+
+    obyek = &b;
+    obyek->pesan();
+
+    return 0;
+}
