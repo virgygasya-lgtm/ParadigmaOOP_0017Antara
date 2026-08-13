3f4cb04cbb708d4c60d0ca4428d5b3b98a02d50a
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:04:52 +0700

    mengisidatasaklar.cpp

--- mengisidatasaklar.cpp
+++ mengisidatasaklar.cpp
@@ -56,3 +56,27 @@ private:
 
 int main() {
     remoteLampu lampuRumah;
+
+    #include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[5];
+
+    public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
+        string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
