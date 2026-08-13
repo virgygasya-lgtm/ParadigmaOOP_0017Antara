790ba73cdc794a2e5c7c205c373b0faa3e0ca76e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:03:46 +0700

    membuatobjeklampu.cpp

--- membuatobjeklampu.cpp
+++ membuatobjeklampu.cpp
@@ -36,3 +36,23 @@ private:
         return saklarNo[i];
     }
 };
+
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[5];
+
+    public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
+        string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
