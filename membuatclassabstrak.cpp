86a30cb148ce7fd0223f4292637d6b9449d2b15c
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:36:46 +0700

    membuatclassjoko.cpp

--- membuatclassabstrak.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-class seseorang {
-public:
-    virtual void pesan() = 0;
-};
---
+++ membuatclassjoko.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+};
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
