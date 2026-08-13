d5e9b326f70e51a450413f8a520dfcd6a530d5c4
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:17:01 +0700

    membuatfungsimain.cpp

--- membuatfungsimain.cpp
+++ membuatfungsimain.cpp
@@ -230,3 +230,55 @@ public:
             + "\n\n";
     }
 };
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variable dari class orang
+    string nama;
+
+        //pembuatan constructor dan destructor dari class orang
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+        ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+        //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+    };
+
+//pembuatan class pelajar sebagai pewaris dari class orang
+class pelajar : public orang {
+public:
+    //deklarasi member variable dari class pelajar
+    string sekolah;
+
+        //pembuatan constructor dan destructor dari class pelajar
+    pelajar(string pNama, string pSekolah) :orang(pNama), sekolah
+        (pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+        ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+        //pembuatan function perkenalan()
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah
+            + "\n\n";
+    }
+};
+
+int main()
+{
