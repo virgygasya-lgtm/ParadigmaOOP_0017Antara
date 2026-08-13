ea9061b44ebf2661935e72edd478a27b60497cdd
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:01:30 +0700

    membuatmethodsaklar.cpp

---
+++ membuatmethodsaklar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[5];
+
+    #include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[5];
+
+    public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
