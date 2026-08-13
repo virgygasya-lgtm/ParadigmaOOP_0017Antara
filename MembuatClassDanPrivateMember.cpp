927cd8b09e1a677eef71c3eff899e378033322ba
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 14:54:02 +0700

    MenambahkanMethodUntukMengsisNilai.cpp

--- MembuatClassDanPrivateMember.cpp
+++ MembuatClassDanPrivateMember.cpp
@@ -5,3 +5,27 @@ class AbstraksiKlas {
 private:
     string x, y;
 }; 
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    string x, y;
+}; 
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+};
