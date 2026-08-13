d8ee346beef7f74ee519ce1155cc3d240704048e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:15:11 +0700

    membuatdestructorpelajar.cpp

--- membuatdestructorpelajar.cpp
+++ membuatdestructorpelajar.cpp
@@ -139,3 +139,45 @@ public:
         (pSekolah) {
         cout << "Pelajar dibuat\n" << endl;
     }
+
+    #include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variable dari class orang
+    string nama;
+
+        //pembuatan constructor dan destructor dari class orang
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+        ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+        //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+    };
+
+//pembuatan class pelajar sebagai pewaris dari class orang
+class pelajar : public orang {
+public:
+    //deklarasi member variable dari class pelajar
+    string sekolah;
+
+        //pembuatan constructor dan destructor dari class pelajar
+    pelajar(string pNama, string pSekolah) :orang(pNama), sekolah
+        (pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+        ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
