4d49c6b86e83c12867bb35598f8ede072531e462
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:24:50 +0700

    membuatconstructororang.cpp

---
+++ membuatconstructororang.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    #include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
