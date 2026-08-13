281ac76c5c062a0edc8a29d1a6888ee01bc994de
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 15:31:27 +0700

    membuatderivedclass.cpp

--- membuatbaseclass.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-class baseClass {
-public:
-    virtual void perkenalan() final{
-        cout << "Hallo saya Function dari base class";
-    }
-};
---
+++ membuatderivedclass.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final{
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final{
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
